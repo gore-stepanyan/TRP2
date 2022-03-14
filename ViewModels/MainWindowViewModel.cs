ea7cd41f26f199457450362a6ccf873a80979638
Andrej Mazurov <89501431007and@gmail.com>
2022-03-14 12:40:57 +0700

    mvvm

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRP2.ViewModels.Base;
+
+namespace TRP2.ViewModels
+{
+    internal class MainWindowViewModel:ViewModel
+    {
+    }
+}
